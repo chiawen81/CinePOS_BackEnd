73d2c7d07d57693ca20e389baa51736206a9515f
chiawen81 <fangchiawen@gmail.com>
2023-04-23 23:21:08 +0800

    編譯

--- dist/app.js
+++ dist/app.js
@@ -4,16 +4,16 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const cors_1 = __importDefault(require("cors"));
-const mongoose_1 = __importDefault(require("mongoose"));
 const cookie_parser_1 = __importDefault(require("cookie-parser"));
 const morgan_1 = __importDefault(require("morgan"));
 const express = require('express');
 const path = require('path');
 const app = express();
+const error_1 = __importDefault(require("./service/error"));
 const index_1 = require("./routes/index");
-const users_1 = require("./routes/users");
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+const user_1 = require("./routes/user");
+const log_in_1 = require("./routes/log-in");
+require("./service/connection");
 app.use((0, cors_1.default)());
 app.use((0, morgan_1.default)('dev'));
 app.use(express.json());
@@ -21,17 +21,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use((0, cookie_parser_1.default)());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index_1.indexRouter);
-app.use('/user', users_1.usersRouter);
-const uri = "mongodb+srv://fangchiawen:aass6688@cluster0.t7hdpfl.mongodb.net/hotel?retryWrites=true&w=majority";
-const options = {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-};
-mongoose_1.default.connect(uri, options).then(() => {
-    console.log('MongoDB Atlas connected');
-}).catch(err => {
-    console.log('MongoDB Atlas connection error:', err);
-});
-app.listen(process.env.PORT || 3005, () => {
+app.use('/user', user_1.userRouter);
+app.use('/log-in', log_in_1.logInRouter);
+app.use(error_1.default.catchCustomError);
+app.listen(process.env.LOCAL_PORT || 3005, () => {
     console.log(`Server is running on port ${process.env.PORT || 3005}`);
 });
---
+++ dist/controller/user.js
@@ -0,0 +1,47 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const validator_1 = __importDefault(require("validator"));
+const usersModels_1 = __importDefault(require("../models/usersModels"));
+const error_1 = __importDefault(require("../service/error"));
+class UserController {
+    constructor() {
+        this.changeUserName = (req, res, next) => __awaiter(this, void 0, void 0, function* () {
+            console.log("抓到路由- user/info/update");
+            const { newName, staffId } = req.body;
+            if (!validator_1.default.isLength(newName, { min: 2 })) {
+                return next(error_1.default.appError(400, "姓名欄位驗證錯誤！", next));
+            }
+            ;
+            try {
+                const updatedUser = yield usersModels_1.default.findOneAndUpdate({ staffId }, { name: newName }, { new: true });
+                console.log('updatedUser ', updatedUser);
+                res.writeHead(200, process.env.HEADERS);
+                res.write(JSON.stringify({
+                    message: "已經成功修改姓名!",
+                    data: {
+                        staffId: updatedUser.staffId,
+                        newName: updatedUser.name
+                    }
+                }));
+                res.end();
+            }
+            catch (err) {
+                res.status(500).json({ error: err.message });
+            }
+            ;
+        });
+    }
+}
+exports.default = new UserController();
