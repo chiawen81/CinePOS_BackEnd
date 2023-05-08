9ff7c29d4223aa040ab6288ad55e594810d1dbcb
chiawen81 <fangchiawen@gmail.com>
2023-05-08 23:57:38 +0800

    重新編譯ts

--- dist/controller/common/upload.js
+++ dist/controller/common/upload.js
@@ -14,45 +14,98 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const firebase_admin_1 = __importDefault(require("../../service/firebase-admin"));
 const multer_1 = __importDefault(require("multer"));
-const uuid_1 = __importDefault(require("uuid"));
+const error_1 = __importDefault(require("./../../service/error"));
+const uuid = require('uuid');
 class UploadController {
     constructor() {
-        this.bucket = firebase_admin_1.default.storage().bucket();
-        this.upload = (0, multer_1.default)({
-            limits: {
-                fileSize: 5 * 1024 * 1024,
-            },
-        });
-    }
-    uploadFile(req, res, next, onFinishFunc) {
-        console.log('bucket:', this.bucket);
-        const file = req.file;
-        const uniqueId = uuid_1.default.v4();
-        const fileName = uniqueId + '-' + file.originalname;
-        const blob = this.bucket.file(fileName);
-        const blobStream = blob.createWriteStream();
-        console.log(file);
-        blobStream.on('finish', () => {
-            res.send('上傳成功');
-            const config = {
-                action: 'read',
-                expires: '12-31-2030',
-            };
-            blob.getSignedUrl(config, (err, imgUrl) => __awaiter(this, void 0, void 0, function* () {
-                onFinishFunc();
-                res.send({
-                    message: '上傳成功！',
-                    fileName: fileName,
-                    imgUrl
+        this.upload = (req, res, next) => __awaiter(this, void 0, void 0, function* () {
+            try {
+                console.log('req.file', req.file);
+                const bucket = firebase_admin_1.default.storage().bucket();
+                const file = req.file;
+                const uniqueId = uuid.v4();
+                const fileName = uniqueId + '-' + file.originalname;
+                const blob = bucket.file(fileName);
+                const blobStream = blob.createWriteStream();
+                blobStream.on('finish', () => __awaiter(this, void 0, void 0, function* () {
+                    const config = {
+                        action: 'read',
+                        expires: '12-31-2030',
+                    };
+                    blob.getSignedUrl(config, (err, imgUrl) => __awaiter(this, void 0, void 0, function* () {
+                        console.log('檔案網址', imgUrl);
+                        req.fileData = { fileName, err, imgUrl, file };
+                        next();
+                    }));
+                }));
+                blobStream.on('error', (err) => {
+                    res.status(500).send(`上傳失敗(系統報錯)：${err.message}`);
                 });
-            }));
+                blobStream.end(file.buffer);
+            }
+            catch (err) {
+                res.status(500).json({
+                    code: -1,
+                    message: err.message || "上傳檔案錯誤！"
+                });
+            }
+            ;
         });
-        blobStream.on('error', (err) => {
-            console.error(err);
-            res.status(500).send(`上傳失敗：${err.message}`);
-        });
-        blobStream.end(file.buffer);
+        this.deleteFile = function deleteFile(req, res, next, fileName) {
+            return __awaiter(this, void 0, void 0, function* () {
+                try {
+                    console.log('刪除檔案');
+                    const bucket = firebase_admin_1.default.storage().bucket();
+                    const blob = bucket.file(fileName);
+                    blob.delete().then(() => {
+                        console.log('刪除成功');
+                    }).catch((err) => {
+                        console.log('刪除失敗');
+                    });
+                }
+                catch (err) {
+                    res.status(500).json({
+                        code: -1,
+                        message: err.message || "刪除檔案錯誤！"
+                    });
+                }
+                ;
+            });
+        };
+        this.photoValidator = (req, res, next) => {
+            return (0, multer_1.default)({
+                limits: { fileSize: 1 * 1024 * 1024 },
+                fileFilter: (req, file, callback) => {
+                    if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+                        callback(null, false);
+                        return error_1.default.appError(401, "圖片格式只接受 jpg、jpeg、png！", next);
+                    }
+                    else if (file.size > 1048576) {
+                        callback(null, false);
+                        return error_1.default.appError(401, "上傳檔案限1MB！", next);
+                    }
+                    else {
+                        callback(null, true);
+                    }
+                    ;
+                },
+            }).single("image")(req, res, next);
+        };
+        this.uploadFileValidator = (req, res, next) => {
+            return (0, multer_1.default)({
+                limits: { fileSize: 1 * 1024 * 1024 },
+                fileFilter: (req, file, callback) => {
+                    if (file.size > 1048576) {
+                        callback(null, false);
+                        return error_1.default.appError(401, "上傳檔案限1MB！", next);
+                    }
+                    else {
+                        callback(null, true);
+                    }
+                    ;
+                },
+            }).single("image")(req, res, next);
+        };
     }
-    ;
 }
 exports.default = new UploadController();
