235acb4a9fa8d9dda3b81c62e0e93ff714149c37
EnPei <110892385+LABIBI-LG@users.noreply.github.com>
2023-05-21 23:47:29 +0800

    feat: 完成場次查詢API

--- src/app.ts
+++ src/app.ts
@@ -18,7 +18,8 @@ import { ManagerMovieRouter } from "./routes/v1/manager/movie";
 
 import { StaffIndexRouter } from './routes/v1/staff/index';
 import { StaffTicketTypeRouter } from './routes/v1/staff/ticketType';
-import { StaffSeaRouter } from './routes/v1/staff/seat';
+import { StaffSeatRouter } from './routes/v1/staff/seat';
+import { StaffScheduleRouter } from './routes/v1/staff/schedule';
 
 // 其它
 import "./service/connection";                        // 資料庫設定
@@ -43,7 +44,8 @@ app.use('/v1/staff/user', CommonUserRouter);
 app.use('/v1/staff/login', CommonLogInRouter);
 
 app.use('/v1/staff/ticketType', StaffTicketTypeRouter);
-app.use('/v1/staff/seat', StaffSeaRouter);
+app.use('/v1/staff/seat', StaffSeatRouter);
+app.use('/v1/staff/schedule', StaffScheduleRouter);
 
 // 後台
 app.use('/v1/manager/', ManagerIndexRouter);
